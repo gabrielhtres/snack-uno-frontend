41687d97c1b57a785ab50cc22d270f20734eb379
Eduarda <dudaguadagnin@unochapeco.edu.br>
2022-06-26 22:09:53 -0300

    radio button no card

--- snack-uno-frontd/src/components/molecules/cards/index.js
+++ snack-uno-frontd/src/components/molecules/cards/index.js
@@ -6,11 +6,15 @@ import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import './index.css'
-import Pizza from '../../../assets/pizza.jpg'
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Button from '@mui/material/Button';
+import { blue } from '@mui/material/colors';
+
+
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -26,47 +30,73 @@ type Props = {
   mock?: Array,
 };
 function Cards({ mock }: Props) {
- // console.log('cards', mock)
+  // console.log('cards', mock)
   const [expanded, setExpanded] = React.useState(false);
+  const [value, setValue] = React.useState('');
+
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
   return (
     <div className='Cards'>
-        <Card className='cards-content' >
-          <CardMedia
-            component="img"
-            height="250"
-            image={mock.image}
-            alt="Paella dish"
-            className='cards-imagem'
-          />
+      <Card className='cards-content' >
+        <CardMedia
+          component="img"
+          height="250"
+          image={mock.image}
+          alt="Paella dish"
+          className='cards-imagem'
+        />
 
-          <CardActions disableSpacing className='cards-body'>
-            <div className='cards-text'>
-              <span className='cards-titulo'>{mock.name}</span>
-              <span className='cards-preco'>R${mock.price}</span>
-            </div>
+        <CardActions disableSpacing className='cards-body'>
+          <div className='cards-text'>
+            <span className='cards-titulo'>{mock.name}</span>
+            <span className='cards-preco'>R${mock.price}</span>
+          </div>
 
-            <ExpandMore
-              expand={expanded}
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="show more"
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon sx={{ color: 'white' }} />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit className='cards-expanded'>
+          <CardContent>
+            <RadioGroup
+              aria-labelledby="demo-controlled-radio-buttons-group"
+              name="controlled-radio-buttons-group"
+              value={value}
+              onChange={handleChange}
             >
-              <ExpandMoreIcon sx={{ color: 'white' }}/>
-            </ExpandMore>
-          </CardActions>
-          <Collapse in={expanded} timeout="auto" unmountOnExit className='cards-expanded'>
-            <CardContent>
-              <Typography paragraph>
-                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                aside for 10 minutes.
-              </Typography>
-            </CardContent>
-          </Collapse>
-        </Card>
+              {mock.flavor.map((fl, ix) => (
+
+                <FormControlLabel
+                  value={fl}
+                  control={
+                    <Radio size="small" sx={{
+                      color: blue[50],
+                      '&.Mui-checked': {
+                        color: blue[800],
+                      },
+                    }} />}
+                  label={fl} />
+              ))}
+            </RadioGroup>
+            <Button variant="contained" color="success">
+              Comprar
+            </Button>
+
+          </CardContent>
+        </Collapse>
+      </Card>
     </div>
   );
 }
